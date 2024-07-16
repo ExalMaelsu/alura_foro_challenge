63be368f52b4b2b7282babf3e0086c19f18a061c
Samuel Gómez <79483621+ExalMaelsu@users.noreply.github.com>
2024-07-16 14:32:09 -0600

    Create UserService.java

---
+++ UserService.java
@@ -0,0 +1,13 @@
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
