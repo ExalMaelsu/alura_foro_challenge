7e24cb809f25b054c10902ef8d30b4cebc0e8e6a
Samuel Gómez <79483621+ExalMaelsu@users.noreply.github.com>
2024-07-16 14:32:40 -0600

    Create SecurityConfig.java

---
+++ SecurityConfig.java
@@ -0,0 +1,22 @@
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(username -> userService.findByUsername(username).orElseThrow())
+            .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/auth/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+            .addFilter(new JwtAuthorizationFilter(authenticationManager()));
+    }
+}
